64d6a2c1293f5a9c1b87022fd574c3483de18608
Steven DeTeso <sadeteso@gmail.com>
2023-01-19 19:25:05 -0500

    List function

--- 1_19_23.py
+++ 1_19_23.py
@@ -48,3 +48,44 @@ if __name__ == '__main__':
     # prints the average marks with two decimal places. 
     print("{:.2f}".format(average))
     # the '{:.2f}' is a format specifier that tell python to format the number as a float with 2 decimal places.
+
+
+# Lists 
+# Consider a list (list = []). You can perform the following commands:
+
+# 1. insert i e: Insert integer  at position .
+# 2. print: Print the list.
+# 3. remove e: Delete the first occurrence of integer .
+# 4. append e: Insert integer  at the end of the list.
+# 5. sort: Sort the list.
+# 6. pop: Pop the last element from the list.
+# 7. reverse: Reverse the list.
+
+# Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. Iterate through each command in order and perform the corresponding operation on your list.
+
+# input format: The first line contains an integer, n, denoting the number of commands.
+# Each line i of the n subsequent lines contains one of the commands described above.
+
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+    for i in range(N):
+        command = input().split()
+        #  the split() method is used to separate the command and its arguments into a list, so that they can be easily accessed.
+        if command[0] == "insert":
+            lst.insert(int(command[1]), int(command[2]))
+            # int() function is used to convert the arguments from strings to integers
+        elif command[0] == "print":
+            print(lst)
+        elif command[0] == "remove":
+            lst.remove(int(command[1]))
+        elif command[0] == "append":
+            lst.append(int(command[1]))
+        elif command[0] == "sort":
+            lst.sort()
+        elif command[0] == "pop":
+            lst.pop()
+        elif command[0] == "reverse":
+            lst.reverse()
+        
+        # command[0] is comparying the string name to what was given, command[1] contains the value given after the comand in the input. 
