363e4d46ee92c46fea21340edb07d5571fa825f7
Steven DeTeso <sadeteso@gmail.com>
2023-01-23 23:01:44 -0500

    DRY on pizza

--- 1_23_23.py
+++ 1_23_23.py
@@ -32,16 +32,17 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
 if size == "S":
     bill += 15
-    if add_pepperoni == "Y":
-        bill += 2
 elif size == "M":
     bill += 20
-    if add_pepperoni == "Y":
-        bill += 3
 elif size == "L":
     bill += 25
-    if add_pepperoni == "Y":
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
         bill += 3
+
 if extra_cheese == "Y":
     bill += 1
 
