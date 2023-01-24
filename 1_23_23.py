d0f758f28e1115813de56b9326799d866bdaa8f5
Steven DeTeso <sadeteso@gmail.com>
2023-01-23 22:17:19 -0500

    leap year algo

---
+++ 1_23_23.py
@@ -0,0 +1,17 @@
+# Leap year - Write a program that works out whether if a given year is a leap year. This is how you work out whether if a particular year is a leap year.
+# it is a Leap year on every year that is evenly divisible by 4 
+    # **except** every year that is evenly divisible by 100 
+        # **unless** the year is also evenly divisible by 400
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
