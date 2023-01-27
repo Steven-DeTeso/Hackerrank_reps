313143b6c716129438ed681292cdd697df6aa1e3
Steven DeTeso <sadeteso@gmail.com>
2023-01-26 21:03:07 -0500

    treasure map, nested lists

---
+++ 1_26_23.py
@@ -0,0 +1,20 @@
+# treasure map - You are going to write a program that will mark a spot with an X.
+# In the starting code, you will find a variable called map.
+# This map contains a nested list. When map is printed this is what the nested list looks like:
+# [['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️']]
+# This is a bit hard to work with. So on lines 6 and 23, we've used this line of code print(f"{row1}\n{row2}\n{row3}" to format the 3 lists to be printed as a 3 by 3 square, each on a new line. 
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+
+col = int(position[0])
+row = int(position[1])
+
+map[row-1][col-1] = "x"
+
+print(f"{row1}\n{row2}\n{row3}")
