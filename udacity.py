ca795161bbdc2aa268845eeb96de27ce9c35870f
Steven DeTeso <sadeteso@gmail.com>
2023-02-27 12:15:03 -0500

    intro to course code

--- udacity.py
+++ udacity.py
@@ -53,3 +53,18 @@ print(me.getClassiness())
 me.addItem("bowtie")
 # Should be 15
 print(me.getClassiness())
+
+
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    statement = "I am super excited for this course! " * 5
+    return statement
+
+print(show_excitement())
